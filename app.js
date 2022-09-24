05162dbfb834bc744582966349e7dcd4f4d0e46b
Guilherme Sepulcro <guilhermesepulcro@hotmail.com>
2022-09-24 01:33:02 -0300

    feat: indice e validacao
    Foi adicionado um indice dinamico no localstorage, e foi adicionado uma validacao de campo quando for adicionar a despesa

--- app.js
+++ app.js
@@ -7,7 +7,42 @@ class Despesa{
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados(){
+        for(let i in this){
+            if(this[i] == undefined || this[i] == '' || this[i == null]){
+                return false
+            }
+        }
+        return true
+    }
 }
+
+class Bd{
+
+    constructor(){
+        let id = localStorage.getItem('id')
+
+        if(id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+    
+    getProximoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d){
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa(){
     
@@ -28,9 +63,25 @@ function cadastrarDespesa(){
         valor.value
     )
 
-    gravar(despesa)
-}
+    if(despesa.validarDados()){
+        //bd.gravar(despesa)
+        //dialog de sucesso
+        document.getElementById('modal-titulo').innerHTML = 'Registro inserido com sucesso'
+        document.getElementById('modal-titulo-div').className = 'modal-header text-success'
+        document.getElementById('modal-conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+        document.getElementById('modal-btn').innerHTML = 'Voltar'
+        document.getElementById('modal-btn').className = 'btn btn-success'
 
-function gravar(despesa){
-    localStorage.setItem('despesa', JSON.stringify(despesa))
+        $('#registraDespesa').modal('show')
+    } else {
+        //dialog de erro
+        document.getElementById('modal-titulo').innerHTML = 'Erro na inclusão do registro'
+        document.getElementById('modal-titulo-div').className = 'modal-header text-danger'
+        document.getElementById('modal-conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente'
+        document.getElementById('modal-btn').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal-btn').className = 'btn btn-danger'
+
+        $('#registraDespesa').modal('show')
+    }
+    
 }
